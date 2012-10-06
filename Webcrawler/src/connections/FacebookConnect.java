45474490c524adae11dc486703dcdc557a98e737
Jonathan <jcoop565@gmail.com>
2012-10-05 21:04:41 -0400

    Added imports and basic constructor

--- Webcrawler/src/connections/FacebookConnect.java
+++ Webcrawler/src/connections/FacebookConnect.java
@@ -1,5 +1,18 @@
 package connections;
 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class FacebookConnect {
+	
+	FacebookConnect(String url){
+		try {
+			Document doc = Jsoup.connect(url).get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 
 }
