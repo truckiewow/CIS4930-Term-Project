9435dcee1aa7f0944007283c6cd0490ace549bd8
tubacoopa <jcoop565@gmail.com>
2012-10-12 16:54:05 -0400

    UI question formatting

--- Webcrawler/src/webcrawler/UI.java
+++ Webcrawler/src/webcrawler/UI.java
@@ -5,17 +5,18 @@ public class UI {
 	// Purpose of this class is to take input from user and provide organized output
 	
 	boolean hasBasicSiteUrls;
-	boolean hasFacebookUrl;
-	boolean hasLinkedInUrl;
+	boolean hasFacebookUrls;
+	boolean hasLinkedInUrls;
 	boolean hasUsernames;
 	boolean hasEmails;
 	boolean hasStates;
 	
 	public static void main(String[] args) {
 		
-		// Need to come up with way to take in user-provided information
-		
 		DataCruncher dataCruncher = new DataCruncher();
+		
+		System.out.println("Please enter the number possessed of each data type. Please seperate each with a space before pressing Enter.");
+		System.out.println("Facebook URL, LinkedIn URL, Other URL, Usernames, Emails, States");
 	}
 
 }
