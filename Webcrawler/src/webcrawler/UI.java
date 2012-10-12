1c4b3984167f901a8efae916568b1cf0c14d4ffd
tubacoopa <jcoop565@gmail.com>
2012-10-12 16:45:57 -0400

    Added BasicSiteConnect class, some booleans for initial UI

--- Webcrawler/src/webcrawler/UI.java
+++ Webcrawler/src/webcrawler/UI.java
@@ -4,6 +4,13 @@ public class UI {
 
 	// Purpose of this class is to take input from user and provide organized output
 	
+	boolean hasBasicSiteUrls;
+	boolean hasFacebookUrl;
+	boolean hasLinkedInUrl;
+	boolean hasUsernames;
+	boolean hasEmails;
+	boolean hasStates;
+	
 	public static void main(String[] args) {
 		
 		// Need to come up with way to take in user-provided information
